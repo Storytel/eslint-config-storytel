0604a6e770ef2fb570b8de2f4a0cc1b47871aae0
Alexander Olsson <noseglid@gmail.com>
2016-02-19 12:34:11 +0100

    Initial ESLint configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'extends': [
+    'storytel/possible-errors',
+    'storytel/best-practices',
+    'storytel/strict-mode',
+    'storytel/variables',
+    'storytel/node-js-common-js',
+    'storytel/stylistic-issues',
+    'storytel/ecmascript-6',
+  ],
+  'env': {
+    'es6': true,
+    'node': true,
+    'browser': true
+  }
+};
